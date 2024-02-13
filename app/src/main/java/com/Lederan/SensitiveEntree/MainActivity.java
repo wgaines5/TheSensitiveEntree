409ef27a12660eccfc6f32e69e00a8d7bcc9fd39
Wgaines5 <willie.8chd@gmail.com>
2024-02-13 17:45:08 -0500

    Progress on Home and Menu

--- app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
@@ -1,37 +1,48 @@
 package com.Lederan.SensitiveEntree;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.Lederan.SensitiveEntree.databinding.ActivityMainBinding;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.primary_menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_home:
+                return true;
+            case R.id.menu_profile:
+                return true;
+            case R.id.menu_cooking_tips:
+                return true;
+            case R.id.menu_about:
+                return true;
+            case R.id.search:
+                return true;
+            case R.id.menu_resources:
+                return true;
+            case R.id.menu_ingredient_substitutions:
+                return true;
+            case R.id.menu_pantry:
+                return true;
+            case R.id.menu_settings:
+                return true;
+            case R.id.menu_calorie_counter:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
+        }
+    }
 }
