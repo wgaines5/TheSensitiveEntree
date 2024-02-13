2f612e8c965c264618bba47feee32ca3220a7244
Wgaines5 <willie.8chd@gmail.com>
2024-02-13 17:45:08 -0500

    Progress on Home and Menu

--- app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
@@ -1,79 +1,48 @@
 package com.Lederan.SensitiveEntree;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.Lederan.SensitiveEntree.databinding.ActivityMainBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-{
-    private ActivityMainBinding binding;
-
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.logoutButton);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
 
-        if(user == null)
-       {
-         Intent intent = new Intent(getApplicationContext(), Login.class);
-         startActivity(intent);
-         finish();
-       }
-        else
-        {
-            textView.setText(user.getEmail());
-        }
-
-
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-/*
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-*/
     }
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.primary_menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_home:
+                return true;
+            case R.id.menu_profile:
+                return true;
+            case R.id.menu_cooking_tips:
+                return true;
+            case R.id.menu_about:
+                return true;
+            case R.id.search:
+                return true;
+            case R.id.menu_resources:
+                return true;
+            case R.id.menu_ingredient_substitutions:
+                return true;
+            case R.id.menu_pantry:
+                return true;
+            case R.id.menu_settings:
+                return true;
+            case R.id.menu_calorie_counter:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
+        }
+    }
 }
