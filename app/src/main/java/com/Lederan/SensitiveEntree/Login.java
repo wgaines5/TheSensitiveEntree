f736be64c2e4421fbacab7ebcb24e62f7494214f
leah8196 <leah8196@gmail.com>
2024-02-15 20:42:08 -0600

    Made a funtional button!

--- app/src/main/java/com/Lederan/SensitiveEntree/Login.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/Login.java
@@ -1,8 +1,16 @@
 package com.Lederan.SensitiveEntree;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class Login extends AppCompatActivity {
 
@@ -10,7 +18,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
+    String email = "";
+    String password ="";
+
+    public void onLoginClick(View view) {
+        EditText e = findViewById(R.id.enterEmailAddress);
+        email = e.getText().toString();
+        EditText p= findViewById(R.id.enterPassword);
+        password = p.getText().toString();
+    }
 
 }
