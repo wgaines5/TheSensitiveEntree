f718220a0ba380a3ff4269c68325e98fbe247be7
TheRticDad <108913015+TheRticDad@users.noreply.github.com>
2024-02-17 23:15:46 -0600

    Added in login and logout features along with registration features.

--- app/src/main/java/com/Lederan/SensitiveEntree/Login.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/Login.java
@@ -1,35 +1,97 @@
 package com.Lederan.SensitiveEntree;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Login extends AppCompatActivity
+{
 
-public class Login extends AppCompatActivity {
+    EditText editTextEmail, editTextPassword;
+    Button buttonLogin;
+    FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-    }
+        editTextEmail = findViewById(R.id.enterEmailAddress);
+        editTextPassword = findViewById(R.id.enterPassword);
+        buttonLogin = findViewById(R.id.loginButton);
+        mAuth = FirebaseAuth.getInstance();
 
-    String email = "";
-    String password ="";
+        buttonLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
 
-    public void onLoginClick(View view) {
-        EditText e = findViewById(R.id.enterEmailAddress);
-        email = e.getText().toString();
-        EditText p= findViewById(R.id.enterPassword);
-        password = p.getText().toString();
+                if (TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Login.this, "Enter mail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful())
+                                {
+                                    Toast.makeText(Login.this, "Login Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else
+                                {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "Login failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
-    public void onCreateAccountClick(View view){
+    public void onCreateAccountClick(View view) {
         Intent myIntent = new Intent(Login.this, Registration.class);
         Login.this.startActivity(myIntent);
     }
-
 }
--- app/src/main/java/com/Lederan/SensitiveEntree/Registration.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/Registration.java
@@ -1,19 +1,96 @@
 package com.Lederan.SensitiveEntree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registration extends AppCompatActivity {
 
+    EditText editTextEmail, editTextPassword;
+    Button buttonReg;
+    FirebaseAuth mAuth;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        editTextEmail = findViewById(R.id.getEmail);
+        editTextPassword = findViewById(R.id.getPassword);
+        buttonReg = findViewById(R.id.registationButton);
+        mAuth = FirebaseAuth.getInstance();
+        buttonReg.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Registration.this, "Enter mail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Registration.this, "Account created! ",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(Registration.this, MainActivity.class));
+                                }
+                                else
+                                {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Registration.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
+
+/*
     String fistName = "";
     String lastName ="";
     String emailAddress="";
@@ -26,8 +103,11 @@ public class Registration extends AppCompatActivity {
         EditText c = findViewById(R.id.getConfirmPassword);
         ImageView x = findViewById(R.id.errorX);
 
+
+
         if(p != c){
             x.setVisibility(View.VISIBLE);
         }
     }
+    */
 }
