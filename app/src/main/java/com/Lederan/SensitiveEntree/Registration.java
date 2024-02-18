13300a800bfa8e57fbef8809c1c0676fa983825f
leah8196 <leah8196@gmail.com>
2024-02-18 12:34:31 -0600

    little fixes

--- app/src/main/java/com/Lederan/SensitiveEntree/Registration.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/Registration.java
@@ -6,10 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -82,7 +91,7 @@ public class Registration extends AppCompatActivity {
                                 else
                                 {
                                     // If sign in fails, display a message to the user.
-                                    Toast.makeText(Registration.this, "Authentication failed.",
+                                    Toast.makeText(Registration.this, "Account already registered.",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
@@ -96,4 +105,5 @@ public class Registration extends AppCompatActivity {
         Intent myIntent = new Intent(Registration.this, Login.class);
         Registration.this.startActivity(myIntent);
     }
+
 }
