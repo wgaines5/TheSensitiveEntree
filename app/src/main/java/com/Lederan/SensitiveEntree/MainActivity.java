47bf4db2651a94cfc62f79869104328a80e5861b
Wgaines5 <willie.8chd@gmail.com>
2024-02-21 22:11:19 -0500

    Menu Clickable and about page started

--- app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/MainActivity.java
@@ -2,16 +2,21 @@ package com.Lederan.SensitiveEntree;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 DrawerLayout drawerLayout;
 NavigationView navigationView;
 Toolbar toolbar;
@@ -19,44 +24,33 @@ Toolbar toolbar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
+
         setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
-    /*public boolean onCreateOptionMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.primary_menu,menu);
+@Override
+public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+}
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
         return true;
     }
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.menu_home:
-                return true;
-            case R.id.menu_profile:
-                return true;
-            case R.id.menu_cooking_tips:
-                return true;
-            case R.id.menu_about:
-                return true;
-            case R.id.search:
-                return true;
-            case R.id.menu_resources:
-                return true;
-            case R.id.menu_ingredient_substitutions:
-                return true;
-            case R.id.menu_pantry:
-                return true;
-            case R.id.menu_settings:
-                return true;
-            case R.id.menu_calorie_counter:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-    }*/
 }
