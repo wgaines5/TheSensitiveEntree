104a1da818b14ec98b3f3a5e25c7dd36f5b77921
lilyzer0 <emerson.lilyd@gmail.com>
2024-03-08 12:22:11 -0800

    Merged Working-IngredSubs

--- app/src/main/java/com/Lederan/SensitiveEntree/CalorieCounter.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/CalorieCounter.java
@@ -1,4 +1,22 @@
 package com.Lederan.SensitiveEntree;
 
-public class CalorieCounter {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalorieCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_counter);
+
+    }
+
 }
