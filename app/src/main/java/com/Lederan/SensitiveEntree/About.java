c819f202bde5b3ee25cd11e15fd7a906031151e9
Wgaines5 <willie.8chd@gmail.com>
2024-03-09 18:26:08 -0500

    Home Screen updated with scroll views

--- app/src/main/java/com/Lederan/SensitiveEntree/About.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/About.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -29,6 +30,8 @@ public class About extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
 
         mAuth = FirebaseAuth.getInstance();
+
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
