2e58e038c626f7550e1203e27527bd6682f89960
Wgaines5 <willie.8chd@gmail.com>
2024-03-10 14:37:15 -0400

    update all merged

--- app/src/main/java/com/Lederan/SensitiveEntree/About.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/About.java
@@ -1,41 +1,36 @@
 package com.Lederan.SensitiveEntree;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.content.Intent;
-import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
-import android.view.View;
-import com.google.android.material.internal.NavigationMenu;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class About extends AppCompatActivity {
-
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     //Define menu Constants
     FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_activity_about);
-
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
         mAuth = FirebaseAuth.getInstance();
+
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -49,24 +44,29 @@ public class About extends AppCompatActivity {
                 } else if (id == R.id.nav_profile) {
                     return false;
                 } else if (id == R.id.search) {
-                    return false;
+                    Intent intent = new Intent(About.this, Search.class);
+                    startActivity(intent);
                 } else if (id == R.id.nav_ingredient_substitutions) {
-                    return false;
+                    Intent intent = new Intent(About.this, IngredientSubs.class);
+                    startActivity(intent);
                 } else if (id == R.id.nav_calorie_counter) {
-                    return false;
+                    Intent intent = new Intent(About.this, CalorieCounter.class);
+                    startActivity(intent);
                 } else if (id == R.id.nav_pantry) {
-                    return false;
+                    Intent intent = new Intent(About.this, RecipeListings.class);
+                    startActivity(intent);
                 } else if (id == R.id.nav_cooking_tips) {
                     return false;
                 } else if (id == R.id.nav_resources) {
                     return false;
                 } else if (id == R.id.nav_settings) {
                     return false;
-
-                }else if (id == R.id.nav_logout){
+                }else if (id == R.id.nav_logout) {
                     mAuth.signOut();
                     Intent intent = new Intent(About.this, Login.class);
                     startActivity(intent);
+                }else {
+                    return false;
                 }
                 drawerLayout.closeDrawer(GravityCompat.START);
                 return true;
