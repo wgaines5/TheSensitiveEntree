30d475338734cb86c0d2e60dfef1bd5c82e32241
lilyzer0 <emerson.lilyd@gmail.com>
2024-03-10 12:03:52 -0700

    Added resources and cooking tips pages

---
+++ app/src/main/java/com/Lederan/SensitiveEntree/CookingTips.java
@@ -0,0 +1,15 @@
+package com.Lederan.SensitiveEntree;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CookingTips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cooking_tips);
+
+    }
+}
---
+++ app/src/main/java/com/Lederan/SensitiveEntree/Resources.java
@@ -0,0 +1,18 @@
+package com.Lederan.SensitiveEntree;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Resources extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+
+    }
+}
