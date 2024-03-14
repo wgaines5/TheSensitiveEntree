44bdd1919709cd05faa2d2fc4ce168695d3bfb7a
lilyzer0 <emerson.lilyd@gmail.com>
2024-03-14 02:47:02 -0700

    Created layout for spinners and redid dropdown background

--- app/src/main/java/com/Lederan/SensitiveEntree/IngredientSubs.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/IngredientSubs.java
@@ -105,81 +105,81 @@ public class IngredientSubs extends AppCompatActivity implements AdapterView.OnI
         //Dropdown #1
         sub1_spinner = (Spinner)findViewById(R.id.sub1_spinner);
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs1);
+                R.layout.spinner_layout,subs1);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub1_spinner.setAdapter(adapter);
         sub1_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #2
         sub2_spinner = (Spinner)findViewById(R.id.sub2_spinner);
         ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs2);
+                R.layout.spinner_layout,subs2);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub2_spinner.setAdapter(adapter);
         sub2_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #3
         sub3_spinner = (Spinner)findViewById(R.id.sub3_spinner);
         ArrayAdapter<String>adapter3 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs3);
+                R.layout.spinner_layout,subs3);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub3_spinner.setAdapter(adapter);
         sub3_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #4
         sub4_spinner = (Spinner)findViewById(R.id.sub4_spinner);
         ArrayAdapter<String>adapter4 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs4);
+                R.layout.spinner_layout,subs4);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub4_spinner.setAdapter(adapter);
         sub4_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #5
         sub5_spinner = (Spinner)findViewById(R.id.sub5_spinner);
         ArrayAdapter<String>adapter5 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs5);
+                R.layout.spinner_layout,subs5);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub5_spinner.setAdapter(adapter);
         sub5_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #6
         sub6_spinner = (Spinner)findViewById(R.id.sub6_spinner);
         ArrayAdapter<String>adapter6 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs6);
+                R.layout.spinner_layout,subs6);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub6_spinner.setAdapter(adapter);
         sub6_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #7
         sub7_spinner = (Spinner)findViewById(R.id.sub7_spinner);
         ArrayAdapter<String>adapter7 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs7);
+                R.layout.spinner_layout,subs7);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub7_spinner.setAdapter(adapter);
         sub7_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #8
         sub8_spinner = (Spinner)findViewById(R.id.sub8_spinner);
         ArrayAdapter<String>adapter8 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs8);
+                R.layout.spinner_layout,subs8);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub8_spinner.setAdapter(adapter);
         sub8_spinner.setOnItemSelectedListener(this);
 
         //Dropdown #9
         sub9_spinner = (Spinner)findViewById(R.id.sub9_spinner);
         ArrayAdapter<String>adapter9 = new ArrayAdapter<String>(com.Lederan.SensitiveEntree.IngredientSubs.this,
-                android.R.layout.simple_spinner_item,subs9);
+                R.layout.spinner_layout,subs9);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
         sub9_spinner.setAdapter(adapter);
         sub9_spinner.setOnItemSelectedListener(this);
 
