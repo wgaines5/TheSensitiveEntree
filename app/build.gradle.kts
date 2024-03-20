c2b5ca2b75f79bf5cd2e6379c6c746b4e9d7552e
leah8196 <leah8196@gmail.com>
2024-03-20 18:34:36 -0500

    Gradle dependencies added to correct firebase issues in registration and main activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,14 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.10.3")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-database")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // Needed for FireBase
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor("com.github.bumptech.glide:glide:4.14.2")
 }
