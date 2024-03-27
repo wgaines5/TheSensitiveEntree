b83a91bcbd3a8b3ec68d0b45a0bdc477c744cb6b
leah8196 <leah8196@gmail.com>
2024-03-26 20:49:53 -0500

    added dietary preferences onto profile page. removed  implementation("com.google.github.bumptech.glide:glide:4.14.2") from gradle due to build errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,6 @@ dependencies {
     // Needed for FireBase
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.github.bumptech.glide:glide:4.14.2")
+
     annotationProcessor("com.github.bumptech.glide:glide:4.14.2")
 }
