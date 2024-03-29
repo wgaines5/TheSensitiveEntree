821fd1e671691cffa7545b2f75ba4c091a754424
leah8196 <leah8196@gmail.com>
2024-03-28 23:45:51 -0500

    Fixed missing toolbars on resouces, RecipeListings pages

--- app/src/main/java/com/Lederan/SensitiveEntree/RecipeListings.java
+++ app/src/main/java/com/Lederan/SensitiveEntree/RecipeListings.java
@@ -14,6 +14,7 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -43,12 +44,33 @@ public class RecipeListings extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_activity_recipelistings);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.recipeList_drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.recipeListToolbar);
 
         mAuth = FirebaseAuth.getInstance();
 
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navMenu();
+
+    }
+
+
+    public void onMenuClick(View view)
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+    public void navMenu(){
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
